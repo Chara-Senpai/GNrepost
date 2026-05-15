1ca9807a7ad106a72cd31345e4b4f70221dc0d4b
Sev <132365095+Chara-Senpai@users.noreply.github.com>
2026-05-15 20:00:36 +0800

    retconned

--- Assets/Script/TopDownCameraFollow.cs
+++ Assets/Script/TopDownCameraFollow.cs
@@ -15,11 +15,10 @@ public class TopDownCameraFollow : MonoBehaviour
     private void LateUpdate()
     {
         if (target == null) return;
-        Vector3 desiredPosition = target.position + offset;
 
-        Vector3 smoothedPosition = 
-            Vector3.Lerp(transform.position, desiredPosition, followSpeed * Time.deltaTime);
-        target.position = smoothedPosition;
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, followSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
 
         transform.LookAt(target);
     }
